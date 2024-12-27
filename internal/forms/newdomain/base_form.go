43d46018725678fe4737454235b382c433511295
Eduardo Aguilar <dante.aguilar41@gmail.com>
2024-12-27 17:41:11 -0600

    chore: save

--- internal/forms/newdomain/base_form.go
+++ internal/forms/newdomain/base_form.go
@@ -9,14 +9,14 @@ import (
 	"golang.org/x/text/language"
 
 	"github.com/Drafteame/draft/internal/dtos"
-	inputs2 "github.com/Drafteame/draft/internal/pkg/inputs"
+	"github.com/Drafteame/draft/internal/pkg/inputs"
 )
 
 func baseForm(input *dtos.DomainInput) error {
-	err := inputs2.Text("Domain Path:",
-		inputs2.WithValue(&input.DomainPath),
-		inputs2.WithDescription[string]("Enter the path to the domain folder."),
-		inputs2.WithValidation(func(val string) error {
+	err := inputs.Text("Domain Path:",
+		inputs.WithValue(&input.DomainPath),
+		inputs.WithDescription[string]("Enter the path to the domain folder."),
+		inputs.WithValidation(func(val string) error {
 			if val == "" {
 				return errors.New("domain path cannot be empty")
 			}
@@ -41,10 +41,10 @@ func baseForm(input *dtos.DomainInput) error {
 		input.DomainPath = "domains/" + input.DomainPath
 	}
 
-	err = inputs2.Select[string]("Select DB Type:",
-		inputs2.WithDescription[string]("Select the type of database you want to use"),
-		inputs2.WithValue(&input.DBType),
-		inputs2.WithOptions(map[string]string{
+	err = inputs.Select[string]("Select DB Type:",
+		inputs.WithDescription[string]("Select the type of database you want to use"),
+		inputs.WithValue(&input.DBType),
+		inputs.WithOptions(map[string]string{
 			"Postgres": "postgres",
 		}),
 	)
