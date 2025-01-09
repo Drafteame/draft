202c5f592f9fd3672b719ee00cb81a92718cd43f
Eduardo Aguilar <dante.aguilar41@gmail.com>
2025-01-08 20:13:47 -0600

    chore: add flake project with hash auto cretion

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Draft CLI tool";
+
+  inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+      flake-utils.url = "github:numtide/flake-utils";
+    };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        draftVersion = "1.6.0";
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          pname = "draft";
+          version = draftVersion;
+
+          subPackages = [ "." ];
+
+          src = ./.;
+
+          ldflags = [
+            "-s"
+            "-w"
+            "-X 'github.com/Drafteame/draft/cmd/commands.Version=nix-v${draftVersion}'"
+          ];
+
+          env.CGO_ENABLED = false;
+          env.GOWORK = "off";
+
+          vendorHash = "sha256-e324nw0xAfFE1jC1oBXpDuWLtlIOXRPg6LUSZyCbfII=";
+
+          meta = {
+            description = "CLI tool for creating services and lambdas on Draftea monorepo";
+            mainProgram = "draft";
+          };
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
+      }
+    );
+}
