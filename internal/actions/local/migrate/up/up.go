ead8e98154910d8709c21bb0d3ab0f2ef646d00a
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2025-03-26 17:06:32 -0600

    refactor: migrate up (#84)

---
+++ internal/actions/local/migrate/up/up.go
@@ -0,0 +1,16 @@
+package up
+
+type Input struct {
+	WorkingDir         string
+	Database           string
+	LocalMigrateConfig string
+	All                bool
+}
+
+type Action struct {
+	Input Input
+}
+
+func New(input Input) *Action {
+	return &Action{input}
+}
