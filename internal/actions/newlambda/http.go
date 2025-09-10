98b5da2d19c0a688e1dc9021b70c2d968145d71c
arielsantos <arielg.santos21@gmail.com>
2025-09-10 10:47:30 -0300

    chore: add new lambda structure to http lambdas

--- internal/actions/newlambda/http.go
+++ internal/actions/newlambda/http.go
@@ -10,13 +10,26 @@ func (nl *NewLambda) createHttp() error {
 		return err
 	}
 
+	if err := dirs.Create(nl.lambdaPath + "/handler/worker"); err != nil {
+		return err
+	}
+
+	if err := dirs.Create(nl.lambdaPath + "/handler/embed"); err != nil {
+		return err
+	}
+
+	if err := dirs.Create(nl.lambdaPath + "/handler/dtos"); err != nil {
+		return err
+	}
+
 	filesEntries := []dtos.FileEntry{
 		{Path: "/main.go", Data: nl.tmpl.HTTP.MainGo},
 		{Path: "/lambda-config.yml", Data: nl.tmpl.HTTP.LambdaConfigYAML},
-		{Path: "/handler/handler.go", Data: nl.tmpl.HTTP.Handler.HandlerGo},
 		{Path: "/handler/bootstrap.go", Data: nl.tmpl.HTTP.Handler.BootstrapGo},
-		{Path: "/handler/provider.go", Data: nl.tmpl.HTTP.Handler.ProviderGo},
-		{Path: "/handler/route.go", Data: nl.tmpl.HTTP.Handler.RouteGo},
+		{Path: "/handler/worker/worker.go", Data: nl.tmpl.HTTP.Handler.WorkerGo},
+		{Path: "/handler/worker/resources.go", Data: nl.tmpl.HTTP.Handler.ResourcesGo},
+		{Path: "/handler/embed/_.yaml", Data: nl.tmpl.HTTP.Handler.EmbedYML},
+		{Path: "/handler/dtos/dto.go", Data: nl.tmpl.HTTP.Handler.DtosGo},
 	}
 
 	return nl.createFiles(filesEntries...)
