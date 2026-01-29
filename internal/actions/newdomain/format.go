266d52713d181025410cd1321253e6841937c323
Nicolás García <58498506+negarciacamilo@users.noreply.github.com>
2026-01-29 16:29:27 -0300

    feat: enhance formatting functions to support dynamic paths for code generation [DEV-409] (#190)

--- internal/actions/newdomain/format.go
+++ internal/actions/newdomain/format.go
@@ -1,9 +1,17 @@
 package newdomain
 
 import (
+	"github.com/Drafteame/draft/internal/data"
 	"github.com/Drafteame/draft/internal/pkg/format"
 )
 
 func (nd *NewDomain) format() error {
-	return format.Code()
+	paths := []string{nd.input.DomainPath}
+
+	// For Postgres, also format the modified provider files
+	if nd.input.DBType == data.DBTypePostgres {
+		paths = append(paths, "pkg/providers")
+	}
+
+	return format.Code(paths...)
 }
